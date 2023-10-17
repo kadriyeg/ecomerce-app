ac248b25b05c5ca5cf120e7c6b1b02ea952b5c46
kadriyeg <kadriye.sinemuse@gmail.com>
2023-10-17 20:43:05 +0300

    aboutUs part created

--- src/components/aboutUs/AboutUs.jsx
+++ src/components/aboutUs/AboutUs.jsx
@@ -1,17 +1,59 @@
 import React from "react";
+import about1 from "../../assets/about1.jpg"
+import about2 from "../../assets/about2.jpg"
+import about3 from "../../assets/about3.jpg"
+import about4 from "../../assets/about4.jpg"
 
 const AboutUs = () => {
     return (
-        <div>
-            <div className="font-bold text-5xl mt-8">
+        <div className="mx-10">
+            <div className="font-extrabold text-5xl mt-8">
                 About us
             </div>
-            <div className='flex justify-start mb-1 '>
-          Comfort is the key and how you feel about your chair will
-       </div>
-       <div className='flex justify-start'>
-       affect how you feel and your mood.
-       </div>
+            <div className='flex justify-start mt-1 text-gray-500'>
+                Comfort is the key and how you feel about your chair will
+            </div>
+            <div className='flex justify-start text-gray-500'>
+                affect how you feel and your mood.
+            </div>
+            <div class="grid grid-cols-3">
+                <div class="grid grid-cols-2 gap-20 place-content-start">
+                    <div>
+                        <div className="text-6xl text-orange-600">1</div>
+                        <div className="font-bold text-2xl">Mission</div>
+                        <div className="text-gray-500">It makes people at ease and feel like they want to open up.</div>
+                    </div>
+                    <div>
+                        <div className="text-6xl text-orange-600">2</div>
+                        <div className="font-bold text-2xl">Motivation</div>
+                        <div className="text-gray-500">It makes people at ease and feel like they want to open up.</div>
+                    </div>
+                    <div>
+                        <div className="text-6xl text-orange-600">3</div>
+                        <div className="font-bold text-2xl">Vision</div>
+                        <div className="text-gray-500">It makes people at ease and feel like they want to open up.</div>
+
+                    </div>
+                    <div>
+                        <div className="text-6xl text-orange-600">4</div>
+                        <div className="font-bold text-2xl">Goal</div>
+                        <div className="text-gray-500">It makes people at ease and feel like they want to open up.</div>
+                    </div>
+                </div>
+                <div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                
+                </div>
+                <div class="grid grid-cols-2 gap-20 place-content-end">
+                    <div>
+                        <img src={about1}></img>
+                    </div>
+                    <div><img src={about2}></img></div>
+                </div>
+            </div>
         </div>
     )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,15 @@
 import React from 'react';
 import HomeTitle from '../components/homeTitle/HomeTitle';
 import HomeSearch from '../components/homeSearch/HomeSearch';
-import couch from '../assets/couch.png';
 import AboutUs from '../components/aboutUs/AboutUs';
+import Products from '../components/products/Products';
 const Home = () => {
     return (
         <div>
             <HomeTitle />
             <HomeSearch />
-            <div className='flex justify-center items-center  m-0'>
-                <img src={couch} alt="couch" className='w-full h-auto' />
-            </div>
             <AboutUs />
+            <Products />
         </div>
 
 
